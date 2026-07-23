3daa58d6a4dfa9fd7d583f92d243d7bdd7fe5697
devkevbot <31908183+devkevbot@users.noreply.github.com>
2026-07-23 16:55:28 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        // 1. Sort each input string
+        // 2. Create map of sorted input => unsorted input
+        // 3. Return map.values()
+
+        // Complexity:
+        // k = number of strings
+        // n = max length of string
+        // Time to sort one string  =  n log n
+        // Time to sort all strings =  k n log n
+        // Space: map = k values * n = k n
+
+        const map = {};
+
+        for (const unsortedValue of strs) {
+            const letters = Array.from(unsortedValue);
+            letters.sort();
+            const sortedValue = letters.join("");
+            if (!(sortedValue in map)) {
+                map[sortedValue] = [];
+            }
+            map[sortedValue].push(unsortedValue);
+        }
+
+        return Object.values(map);
+    }
+}
