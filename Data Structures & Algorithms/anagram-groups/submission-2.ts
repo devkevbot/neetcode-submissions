4a63384b49f04a0a7b5910e34da752f85a5b358b
devkevbot <31908183+devkevbot@users.noreply.github.com>
2026-07-23 17:05:03 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        // Let k = number of strings
+        // Let n = length of longest string
+        //
+        // Time: k * n
+        // Space: k * n
+
+        const result = {};
+
+        for (const s of strs) {
+            const count = new Array(26).fill(0);
+
+            for (const c of s) {
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            }
+
+            const key = count.join(",");
+            if (!result[key]) result[key] = [];
+            result[key].push(s);
+        }
+
+        return Object.values(result);
+    }
+}
