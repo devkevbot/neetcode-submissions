27de708a590b8499baa8eccb3e97229069f52265
devkevbot <31908183+devkevbot@users.noreply.github.com>
2026-07-24 18:19:19 -0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.ts
@@ -0,0 +1,72 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        // Initial thoughts:
+        //
+        // Keep a set for each row, column, and box.
+        //
+        // Iterate through rows:
+        // - If value in column is a number:
+        //   - If seen in row's set: return false
+        //   - Else: record in row's set
+        //
+        // Iterate through columns:
+        // - If value in row is a number:
+        //   - If seen in col's set: return false
+        //   - Else: record in col's set
+        //
+        // Iterate through boxes (not sure how):
+        // - If value in (row, col) is a number:
+        //  - if seen in box's set: return false
+        // -  else: record in box's set
+        //
+        // return true
+        //
+        // Let n = side length of board
+        // Time: O(n^2)
+        // Space: O(n^2)
+        const BOARD_SIDE_LENGTH = 9;
+
+        const rowSets = Array.from({ length: BOARD_SIDE_LENGTH }, () => new Set<string>());
+        const colSets = Array.from({ length: BOARD_SIDE_LENGTH }, () => new Set<string>());
+        const boxSets = Array.from({ length: BOARD_SIDE_LENGTH }, () => new Set<string>());
+
+        // Check rows
+        for (let i = 0; i < BOARD_SIDE_LENGTH; i++) {
+            for (let j = 0; j < BOARD_SIDE_LENGTH; j++) {
+                const value = board[i][j];
+                if (value === ".") continue;
+                if (rowSets[i].has(value)) return false;
+                rowSets[i].add(value);
+            }
+        }
+
+        // Check columns
+        for (let i = 0; i < BOARD_SIDE_LENGTH; i++) {
+            for (let j = 0; j < BOARD_SIDE_LENGTH; j++) {
+                const value = board[j][i];
+                if (value === ".") continue;
+                if (colSets[i].has(value)) return false;
+                colSets[i].add(value);
+            }
+        }
+
+         // Check boxes
+         for (let i = 0; i < BOARD_SIDE_LENGTH; i++) {
+            for (let j = 0; j < BOARD_SIDE_LENGTH; j++) {
+                // Note: I had to use a hint to find the formula here
+                const boxIndex = Math.trunc(i / 3) * 3 + Math.trunc(j / 3);
+
+                const value = board[i][j];
+                if (value === ".") continue;
+                if (boxSets[boxIndex].has(value)) return false;
+                boxSets[boxIndex].add(value);
+            }
+        }
+
+        return true;
+    }
+}
