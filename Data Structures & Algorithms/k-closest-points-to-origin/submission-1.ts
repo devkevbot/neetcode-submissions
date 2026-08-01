40a82cd6574b24e6e513d2cc58e39fdbbf08ccc2
devkevbot <31908183+devkevbot@users.noreply.github.com>
2026-07-31 20:53:46 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points: number[][], k: number): number[][] {
+        // Create a max heap
+        // For each point
+        //  calculate the distance and store [coords, distance] onto the heap
+        // if heap is > k then pop from heap
+
+        const pq = new MaxPriorityQueue((value) => value[1]);
+
+        for (const [x, y] of points) {
+            const dist = Math.sqrt(x * x + y * y);
+            pq.push([[x,y], dist])
+
+            if (pq.size() > k) {
+                pq.pop()
+            }
+        }
+
+        return pq.toArray().map((val) => val[0]);
+    }
+}
