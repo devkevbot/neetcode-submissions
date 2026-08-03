5f0eb9cfcd151bb016063b9333da28bc3052644c
devkevbot <31908183+devkevbot@users.noreply.github.com>
2026-08-03 11:28:10 -0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.ts
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles: number[], h: number): number {
+        let minSpeed = Math.max(...piles);
+
+        let l = 1;
+        let r = minSpeed;
+
+        while (l < r) {
+            let k = Math.trunc((l + r) / 2);
+            const hours = this.hoursToEat(piles, k);
+
+            // Need to eat faster
+            if (hours > h) {
+                l = k + 1;
+            } else { // Could eat slower
+                r = k
+                minSpeed = Math.min(minSpeed, k);
+            }
+        }
+
+        // for (let k = minSpeed; k > 0; k--) {
+        //     const hours = this.hoursToEat(piles, k);
+        //     if (hours > h) break;
+
+        //     minSpeed = Math.min(minSpeed, k);
+        // }
+
+
+        return minSpeed;
+    }
+
+    private hoursToEat(piles: number[], speed: number): number {
+        const hours = piles.reduce((acc, pile) => {
+            return acc + Math.ceil(pile / speed);
+        }, 0);
+
+        return hours;
+    }
+}
